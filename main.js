8caf5b8d17f47f4a75713962fbb9cd8d887047ce
Yatlow <yisrael@atlow.co.il>
2024-12-19 13:58:59 +0200

    to do list

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 "use strict";
 
-const TDetailsBox=document.getElementById("TDetails");
-const TDateBox=document.getElementById("TDate");
-const TTimeBox=document.getElementById("TTime");
+const task_details_box=document.getElementById("task_details_box");
+const task_date_box=document.getElementById("task_date_box");
+const task_time_box=document.getElementById("task_time_box");
 let notes=[];
-let IdCount=0;
+let idCount=0;
 initializeNotes();
 
 function initializeNotes(){
     notes=JSON.parse(localStorage.getItem("notes"));
     if (notes){
         for (let note of notes){
-            IdCount=note.ID>=IdCount?note.ID+1:IdCount;
+            idCount=note.ID>=idCount?note.ID+1:idCount;
         }
         appendNotes();
     }else{
@@ -27,45 +27,41 @@ function AddNote(){
 };
 
 function AddNoteData(){
-    const NoteDate= new Date(TDateBox.value).toLocaleDateString();
-    const alertBox=document.getElementById("alertBox");
-    const alertBoxText=document.getElementById("alertBoxText");
-    const NoteTime= TTimeBox.value;
-    if (!TDetailsBox.value){
-        alertBox.classList.remove("hiddenAllert");
-        alertBoxText.innerHTML="you must add details for your task!";
+    const alert_box=document.getElementById("alertBox");
+    const alert_box_text=document.getElementById("alertBoxText");
+    const details=task_details_box.value;
+    const date= new Date(task_date_box.value).toLocaleDateString();
+    const time= task_time_box.value;
+    if (!details){
+        alert_box.classList.remove("hiddenAllert");
+        alert_box_text.innerHTML="you must add details for your task!";
         return;
     }
-    if (!TDateBox.value){
-        alertBox.classList.remove("hiddenAllert");
-        alertBoxText.innerHTML="you must add a Date for your task!";
+    if (!task_date_box.value){
+        alert_box.classList.remove("hiddenAllert");
+        alert_box_text.innerHTML="you must add a Date for your task!";
         return;
     }
-    if (!TTimeBox.value){
-        alertBox.classList.remove("hiddenAllert");
-        alertBoxText.innerHTML="you must add a time for your task!";
+    if (!task_time_box.value){
+        alert_box.classList.remove("hiddenAllert");
+        alert_box_text.innerHTML="you must add a time for your task!";
         return;
     }
-    const note={
-        ID:IdCount,
-        details:TDetailsBox.value,
-        Date: NoteDate,
-        Time:NoteTime,
-    }
-    IdCount++;
+    const note={idCount,details,date,time,}
+    idCount++;
     notes.push(note);
     updateData();
 }
 
 function updateData(){
-    const StrigfyiedNotes =JSON.stringify(notes)
-    localStorage.setItem("notes",StrigfyiedNotes)
+    const strigfyied_notes =JSON.stringify(notes)
+    localStorage.setItem("notes",strigfyied_notes)
 };
 
 function clearForm(){
-    TDetailsBox.value="";
-    TDateBox.value="";
-    TTimeBox.value="";
+    task_details_box.value="";
+    task_date_box.value="";
+    task_time_box.value="";
 }
 
 function closeAllert(){
@@ -81,17 +77,17 @@ function appendNotes(){
     for (let i = notes.length - 1; i >= 0; i--) {//THIS MAKES SURE THE NEWEST TASK IS ADDED FROM LTR
         let note = notes[i];
         notesBox.innerHTML+=`
-        <div class="Note" id="note${note.ID}">
+        <div class="Note" id="note${note.idCount}">
             <div class="thumbBox">
                 <img src="assets/pictures/thumb.png" class="thumb">
-                <button onclick="deleteNote(${note.ID})" class="hiddenBtn"><span class="bi bi-x-circle-fill" aria-hidden="true"></span></button>
+                <button onclick="deleteNote(${note.idCount})" class="hiddenBtn"><span class="bi bi-x-circle-fill" aria-hidden="true"></span></button>
             </div>
             <div class="TDetails">
                 <span class="AppedTDetails">${note.details}</span>
             </div>
             <div class="TFooter">
-                <span>${note.Date}</span>
-                <span>${note.Time}</span>
+                <span>${note.date}</span>
+                <span>${note.time}</span>
             </div>
         </div>
         `;
