88ac1bb65f9cdbc078010dd939cd4863b42b99bc
Yatlow <yisrael@atlow.co.il>
2024-12-24 15:30:27 +0200

    to do or not to do

--- main.js
+++ main.js
@@ -1,42 +1,44 @@
 "use strict";
 
-const taskDetailsBox=document.getElementById("taskDetailsBox");
-const taskDateBox=document.getElementById("taskDateBox");
-const taskTimeBox=document.getElementById("taskTimeBox");
-let notes=JSON.parse(localStorage.getItem("notes"))||[];
-let idCounter=0;
+const TaskDetailsBox = document.getElementById("taskDetailsBox");
+const TaskDateBox = document.getElementById("taskDateBox");
+const TaskTimeBox = document.getElementById("taskTimeBox");
+const notes = JSON.parse(localStorage.getItem("notes"))||[];
+let idCounter = 0;
 initializePage();
 
 
 function initializePage(){
     for (let note of notes){
-        idCounter=note.idCounter>=idCounter?note.idCounter+1:idCounter;
-    }
+        idCounter = note.idCounter >= idCounter ? note.idCounter+1 : idCounter;
+    };
     appendNotes();
 }
 
 function addNote(){
     addNoteData();
     appendNotes();
-};
+}
 
 function addNoteData(){   
-    const details=taskDetailsBox.value;
-    const date= new Date(taskDateBox.value).toISOString();
-    const time= taskTimeBox.value;
+    const details = TaskDetailsBox.value;
+    let date = new Date(TaskDateBox.value);
+    const time = TaskTimeBox.value;
     if (!details){
-        openAllert("you must add details for your task!",taskDetailsBox);
+        openAlert("you must add details for your task!",TaskDetailsBox);
         return;
     }
-    if (!taskDateBox.value){
-        openAllert("you must add a Date for your task!",taskDateBox);
+    if (!TaskDateBox.value){
+        openAlert("you must add a Date for your task!",TaskDateBox);
+        return;
+    }else{
+        date= date.toISOString();
+    }
+    if (!TaskTimeBox.value){
+        openAlert("you must add a Time for your task!",TaskTimeBox);
         return;
     }
-    if (!taskTimeBox.value){
-        openAllert("you must add a Time for your task!",taskTimeBox);
-        return;
-    }
-    const note={idCounter,details,date ,time,}
+    const note = {idCounter, details, date, time,};
     idCounter++;
     notes.push(note);
     updateData();
@@ -44,86 +46,95 @@ function addNoteData(){
 }
 
 function updateData(){
-    const stringified_notes =JSON.stringify(notes)
-    localStorage.setItem("notes",stringified_notes)
-};
-
-function clearForm(){
-    taskDetailsBox.value="";
-    taskDateBox.value="";
-    taskTimeBox.value="";
-}
-
-function closeAlert(input_to_focus){
-    const alertBox=document.getElementById("alertBox");
-    alertBox.classList.add("hiddenAllert");
-    alertBox.innerHTML=`<span id="alertBoxText"></span>
-    <button id="alertBoxBtn" onclick="closeAlert()">Ok</button>`;
-    if (input_to_focus){
-        input_to_focus.focus();
-        document.getElementById("alertBoxBtn").addEventListener('click',function(){
-            closeAlert()
-        });
-    }
-}
-
-function openAllert(alert_string,input_to_focus){
-    const alert_box=document.getElementById("alertBox");
-    const alert_box_text=document.getElementById("alertBoxText");
-    alert_box.classList.remove("hiddenAllert");
-    alert_box_text.innerHTML=alert_string;
-    document.getElementById("alertBoxBtn").addEventListener('click',function(){
-        closeAlert(input_to_focus)
-    });
+    const stringified = JSON.stringify(notes);
+    localStorage.setItem("notes",stringified);
 }
 
 function appendNotes(){
-    const houseOfCards=document.getElementById("houseOfCards");
-    houseOfCards.innerHTML="";
-    const now= new Date();
+    const houseOfCards = document.getElementById("houseOfCards");
+    houseOfCards.innerHTML = "";
+    const now = new Date();
+    let string = "";
     for (let i = notes.length - 1; i >= 0; i--) {
 // this makes sure the newest task is added on the top left
 // if i misunderstood the task requirements, and the newest task should be appended first (bottom right)>>:
 // i would run the loop this way: for (let i = 0; i <notes.length; i++) {
-        let note = notes[i];
-        const noteDate= new Date(note.date);
-        let dateClass='';
-        let dateString='';
-        if (now>noteDate){
-            dateClass='overdo'
-            dateString='Date has passed:(';
+        const note = notes[i];
+        const noteDate = new Date(note.date);
+        let dateClass = '';
+        let dateString = '';
+        if (now > noteDate){
+            dateClass = 'overdo'
+            dateString = 'Date has passed:(';
         }
-        houseOfCards.innerHTML+=`
-        <div class="Note" id="note${note.idCounter}">
+        string += `
+        <div class="note" id="note${note.idCounter}">
             <div class="thumbBox">
                 <img src="assets/pictures/thumb.png" class="thumb">
                 <button onclick="deleteNote(${note.idCounter})" class="hiddenBtn"><span class="bi bi-x-circle-fill" aria-hidden="true"></span></button>
             </div>
-            <div class="TDetails">
-                <span class="AppedTDetails">${note.details}</span>
+            <div class="taskDetails">
+                <span class="AppendedTaskDetails">${note.details}</span>
             </div>
-            <div class="TFooter">
+            <div class="TaskFooter">
                 <span class="${dateClass}">${noteDate.toLocaleDateString()}  ${dateString}</span>
                 <span>${note.time}</span>
             </div>
         </div>
         `;
     }
+    houseOfCards.innerHTML = string;
+}
+
+function clearForm(){
+    TaskDetailsBox.value = "";
+    TaskDateBox.value = "";
+    TaskTimeBox.value = "";
 }
 
 function deleteNote(uid){
-    const alertBox=document.getElementById("alertBox");
-    for (let i=0; i<notes.length; i++){
-        if (notes[i].idCounter===uid){
-            alertBox.classList.remove("hiddenAllert");
-            alertBox.innerHTML=`<span id="alertBoxText">are you sure you want to delete the task ${notes[i].details}?</span>
-            <button id="alertBoxBtn" onclick="
-            closeAlert();
-            notes.splice(${i},1);
-            updateData();
-            appendNotes();">ok</button>
-            <button id="alertBoxBtn2" onclick="closeAlert()">cancel</button>`;
+    const alertBox = document.getElementById("alertBox");
+    const alertBoxText = document.getElementById("alertBoxText");
+    const alertBoxBtn = document.getElementById("alertBoxBtn");
+    const alertBoxConfirmBtn = document.getElementById("alertBoxConfirmBtn");
+    for (let i = 0; i < notes.length; i++){
+        if (notes[i].idCounter === uid){
+            alertBoxText.innerHTML = "are you sure you want to delete the task"
+            alertBox.classList.remove("hiddenAlert");
+            alertBoxConfirmBtn.classList.remove("hiddenConfirmBtn")
+            alertBoxBtn.innerHTML = "cancel";
+            alertBoxBtn.addEventListener('click',function(){
+                closeAlert();
+            },{once:true});
+            alertBoxConfirmBtn.addEventListener('click',function(){
+                closeAlert();
+                notes.splice(i,1);
+                updateData();
+                appendNotes();
+            },{once:true});
             break;
         }
     }
+}
+
+function closeAlert(inputToFocus){
+    const alertBox = document.getElementById("alertBox");
+    alertBox.classList.add("hiddenAlert");
+    if (inputToFocus){
+        inputToFocus.focus();
+    }
+}
+
+function openAlert(alertString,inputToFocus){
+    const alertBox = document.getElementById("alertBox");
+    const alertBoxText = document.getElementById("alertBoxText");
+    const alertBoxBtn = document.getElementById("alertBoxBtn");
+    const alertBoxConfirmBtn = document.getElementById("alertBoxConfirmBtn");
+    alertBox.classList.remove("hiddenAlert");
+    alertBoxText.innerHTML = alertString;
+    alertBoxBtn.innerHTML="Ok";
+    alertBoxBtn.addEventListener('click',function(){
+        closeAlert(inputToFocus);
+    },{once:true});
+    alertBoxConfirmBtn.classList.add("hiddenConfirmBtn");
 }
